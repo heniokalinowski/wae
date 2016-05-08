60949c1f3bdc276b0c2b7be791eea2641c743d16
Coolthulhu <Coolthulhu@gmail.com>
2016-05-08 20:50:57 +0200

    Zastosowanie normalizacji

--- main.r
+++ main.r
@@ -2,39 +2,68 @@
 source("neural.r")
 source("mutations.r")
 source("selection.r")
+source("normalization.r")
 
-options(error = recover)
+run_test <- function(input_raw, output_raw, tested_fun, num_iter, err_fun, do_plot) {
+    input_norm_data <- normalize(input_raw)
+    input <- input_norm_data$values
 
-tested_fun <- function(x) return(sin(x))
+    output_norm_data <- normalize(output_raw)
+    output <- output_norm_data$values
 
-input <- t(seq(0,pi,length=20))
-#output <- feed_forward(network, input)
-output <- apply(input, 2, tested_fun)
-#print(output)
+    net_set <- lapply(1:100, function(x) return(gen_network(1, 3, 1)))
+    first <- net_set[[1]]
 
-net_set <- lapply(1:100, function(x) return(gen_network(1, 10, 1)))
-first <- net_set[[1]]
+    if(missing(do_plot)) {
+        do_plot <- TRUE
+    }
 
-plot(input, output, type="l", col="green")
-par(new=TRUE)
-predicted <- feed_forward(first, input)
-plot(input, predicted, type="l", col="red", xlab="", ylab="")
+    if(do_plot) {
+        plot(input_raw, output_raw, type="l", col="green")
+        par(new=TRUE)
+        predicted_normalized <- feed_forward(first, input)
+        predicted <- denormalize3(output_norm_data$minimum, output_norm_data$maximum, predicted_normalized)
+        plot(input_raw, predicted, type="l", col="red", xlab="", ylab="")
+    }
 
-for(iter in 1:1000) {
-#if(FALSE){
-    net_set <- new_pop(net_set, input, output)
-    # Zachowujemy najlepszego
-    best_from_set <- net_set[[1]]
-    # Nie mutujemy najlepszego
-    net_set <- mutate_set(net_set[2:length(net_set)])
-    # Dodajemy go z powrotem
-    net_set[[length(net_set) + 1]] <- best_from_set
-    best_output <- feed_forward(best_from_set, input)
-    printf("Iter %d, best error: %.3f\n", iter, get_error(best_from_set, input, output))
-    plot(input, output, type="l", col="green")
-    par(new=TRUE)
-    predicted <- feed_forward(best_from_set, input)
-    plot(input, predicted, type="l", col="red", xlab="", ylab="")
-    #print(best_output)
-    
+    if(missing(err_fun)) {
+        err_fun <- function(x) return(x * x)
+    }
+
+    if(missing(num_iter)) {
+        num_iter <- 100
+    }
+
+    for(iter in 1:num_iter) {
+    #if(FALSE){
+        net_set <- new_pop(net_set, input, output_raw, output_norm_data, err_fun)
+        # Zachowujemy najlepszego
+        best_from_set <- net_set[[1]]
+        # Nie mutujemy najlepszego
+        net_set <- mutate_set(net_set[2:length(net_set)])
+        # Dodajemy go z powrotem
+        net_set[[length(net_set) + 1]] <- best_from_set
+        best_output <- feed_forward(best_from_set, input)
+        printf("Iter %d, best error: %.3f\n", iter, get_error(best_from_set, input, output_raw, output_norm_data, err_fun))
+        predicted_normalized <- feed_forward(best_from_set, input)
+        predicted <- denormalize3(output_norm_data$minimum, output_norm_data$maximum, predicted_normalized)
+
+        if(do_plot) {
+            xlim <- c(min(input_raw), max(input_raw))
+            ylim <- c(min(output_raw, predicted), max(output_raw, predicted))
+            plot(input_raw, output_raw, xlim=xlim, ylim=ylim, type="l", col="green")
+            par(new=TRUE)
+            plot(input_raw, predicted, xlim=xlim, ylim=ylim, type="l", col="red", xlab="", ylab="")
+        }
+    }
+
+    return(best_from_set)
 }
+
+input_raw <- t(seq(2,5,length=20))
+output_raw <- apply(input_raw, 2, tested_fun)
+tested_fun <- function(x) return(x*x*x/exp(x))
+num_iter <- 100
+err_fun <- function(x) return(x * x)
+
+run_test(input_raw, output_raw, tested_fun, num_iter, err_fun, do_plot=TRUE)
--- selection.r
+++ selection.r
@@ -1,22 +1,24 @@
 source("neural.r")
 
-get_error <- function(net, input, output) {
-    predicted <- feed_forward(net, input)
-    err_linear <- abs(predicted - output)
-    #return(sum(err_linear))
-    return(sum(apply(err_linear, c(1, 2), function(x) return(x * x))))
+get_error <- function(net, input_normalized, output_raw, output_norm_data, err_fun) {
+    predicted_normal <- feed_forward(net, input_normalized)
+    predicted <- denormalize3(output_norm_data$minimum, output_norm_data$maximum, predicted_normal)
+    err_linear <- abs(predicted - output_raw)
+    return(sum(apply(err_linear, c(1, 2), err_fun)))
 }
 
-rate <- function(net, input, output) {
-    size_penalty <- 1.0 / (100.0 + 1 * num_hidden(net))
-    error_penalty <- get_error(net, input, output)
+rate <- function(net, input_normalized, output_raw, output_norm_data, err_fun) {
+    size_penalty <- 100.0 + 1 * num_hidden(net)
+    error_penalty <- get_error(net, input_normalized, output_raw, output_norm_data, err_fun)
     return(1.0 / (size_penalty + error_penalty))
 }
 
-new_pop <- function(net_set, input, output) {
-    probs <- lapply(net_set, function(x) return(rate(x, input, output)))
+new_pop <- function(net_set, input_normalized, output_raw, output_norm_data, err_fun) {
+    probs <- lapply(net_set, function(x) return(rate(x, input_normalized, output_raw, output_norm_data, err_fun)))
     rval <- sample(net_set, size = length(net_set) - 1, prob = probs)
     max_index <- which.max(probs)
     rval <- c(net_set[max_index], rval)
     return(rval)
 }
+
+#debug(new_pop)
