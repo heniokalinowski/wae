3251de6b99fad6cf10d6fd0ebed423bd34e3ce35
heniokalinowski <xinus01@gmail.com>
2016-05-10 23:03:56 +0200

    Adding stock.r with data import, however it min(y) gives NA.

---
+++ stock.r
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+
+source("normalization.r")
+source("main.r")
+
+eurpln <- read.csv(file="EUR_PLN_Week1.csv",head=TRUE,sep=",")
+
+x <- as.vector(eurpln$timestamp)
+y <- as.vector(eurpln$RateBid)
+
+print("START")
+print(min(x))
+print("--------------")
+print(min(y))
+print("END")
+
+input_raw <- x
+output_raw <- y
+num_iter <- 100
+err_fun <- function(x) return(x * x)
+do_plot <- TRUE
+
+net_norm <- run_test(input_raw, output_raw, num_iter, err_fun, do_plot)
